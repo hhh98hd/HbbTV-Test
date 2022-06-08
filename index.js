66bdc3a648b919e517fb6ac449ceadc317adb90b
hoanghh <hhh10anhchuyennt@gmail.com>
2022-06-09 00:11:17 +0700

    get the application class

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const appManager = document.getElementById('oipf-app-manager');
 const appManagerObj = window.oipfObjectFactory.createApplicationManagerObject();
 
+for(const key in appManagerObj) {
+    console.log(key)
+}
+
+console.log('----------------------------');
+
+const appClass = appManagerObj.getOwnerApplication(window.document);
 for(const key in appManagerObj) {
     console.log(key)
 }
