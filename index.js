5109c7d33f6bf32345f3876616bbc69bbe775cb3
hoanghh <hhh10anhchuyennt@gmail.com>
2022-06-09 01:05:27 +0700

    .

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const appManagerObj = window.oipfObjectFactory.createApplicationManagerObject();
+// const appManagerObj = window.oipfObjectFactory.createApplicationManagerObject();
+const appManagerObj = document.getElementById('oipf-app-manager');
 
 for(const key in appManagerObj) {
-    console.log(key)
+    console.log(key);
 }
 
 console.log('----------------------------');
 
 const appClass = appManagerObj.getOwnerApplication(document);
-console.log(appClass)
+console.log(appManagerObj.getOwnerApplication);
+console.log(appClass);
 for(const key in appClass) {
-    console.log(key)
+    console.log(key);
 }
