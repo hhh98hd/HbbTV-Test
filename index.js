e7e7396074a9b6c86613fb55a65875ef2b741ced
hoanghh <hhh10anhchuyennt@gmail.com>
2022-06-09 02:32:42 +0700

    Test Low Memory event

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const appManagerObj = window.oipfObjectFactory.createApplicationManagerObject();
 dispatchEvent(new Event('lowmemory'));
 dispatchEvent(new Event('LowMemory'));
 
+console.log(appManagerObj.onlowmemory);
+console.log(appManagerObj.onLowMemory);
+
 appManagerObj.onlowmemory = function() {
     console.log('onlowmemory() called');
 }
